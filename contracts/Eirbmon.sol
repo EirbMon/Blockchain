807182e13d6f321e645f162be815a92ed84924d9
dcognet <dcognet@enseirb-matmeca.fr>
2019-11-05 20:03:32 +0100

    génération aléatoire d'Eirbmon

--- contracts/Eirbmon.sol
+++ contracts/Eirbmon.sol
@@ -8,26 +8,80 @@ contract Eirbmon{
         address owner;
         uint level;
         string field;
-        string[2] atkList;
+        string atk;
         uint hp;
     }
 
-    string[4] private allName = ["roulade","petite bière","brasse","rattrapage"];
-    uint[4] allNameWeight = [10,6,3,1]; // poids entre 1 et 10 avec 1=> très rare
+    string[] private allAtk = ["roulade","petite bière","brasse","rattrapage"];
+    uint[] private allAtkWeight = [0xa,0x6,0x3,0x1]; // poids entre 1 et 10 avec 1=> très rare
 
+    string[] private allField = ["RSI","SEE","Elec","Matmeca","Info","Telecom"];
+    uint[] private allFieldWeight = [0xa,0x8,0x6,0x4,0x2,0x1]; // poids entre 1 et 10 avec 1=> très rare
+ 
+    string[] private allName = ["nom1","nom2","nom3","nom4"];
+    uint[] private allNameWeight = [0xa,0x6,0x3,0x1]; // poids entre 1 et 10 avec 1=> très rare
 
     uint private eirbmonsCount = 0;
+
 
     mapping(uint => _Eirbmon) public _Eirbmons;
 
     constructor () public {
-        addEirbmonToChain("pika",0x0000000000000000000000000000000000000000,"telecom","roulade","rattrapage",100);
+        addEirbmonToChain("pika",0x0000000000000000000000000000000000000000,"telecom","roulade",100);
     }
 
     // ajoute un Eirbmon à la chaine
-    function  addEirbmonToChain(string memory name,address owner, string memory field, string memory atk1,string memory atk2, uint hp) public {
+    function  addEirbmonToChain(string memory name,address owner, string memory field, string memory atk, uint hp) public {
         eirbmonsCount++;
-        _Eirbmons[eirbmonsCount] = _Eirbmon(eirbmonsCount,name,owner,0,field,[atk1,atk2],hp);
+        _Eirbmons[eirbmonsCount] = _Eirbmon(eirbmonsCount,name,owner,0,field,atk,hp);
+    }
+    
+
+    // génère un nouvel Eirbmon random
+    function generateAnNewEirbmon(uint bloc) public {
+        string memory selectedAtk;
+        string memory selectedField;
+        string memory selectedName;
+
+        uint randAtkWeight = uint(uint(blockhash(block.number-bloc))/((0xf+1)**22))%sumWeight(allAtkWeight)+1;
+        uint S1 = 0x0;
+        for(uint i = 0;i < allAtkWeight.length;i++){
+            S1 += allAtkWeight[i];
+            if(randAtkWeight <= S1){
+                selectedAtk=allAtk[i] ;
+                break;
+            }
+        }
+
+        uint randFieldWeight = uint(uint(blockhash(block.number-bloc))/((0xf+1)**44))%sumWeight(allFieldWeight);
+        uint S2 = 0x0;
+        for(uint i = 0;i < allFieldWeight.length;i++){
+            S2 += allFieldWeight[i];
+            if(randFieldWeight <= S2){
+                selectedField = allField[i];
+                break;
+            }
+        }
+
+        uint randNameWeight = uint(uint(blockhash(block.number-bloc)))%sumWeight(allNameWeight);
+        uint S3 = 0x0;
+        for(uint i = 0;i < allNameWeight.length;i++){
+            S3 += allNameWeight[i];
+            if(randNameWeight <= S3){
+                selectedName = allName[i];
+                break;
+            }
+        }
+        addEirbmonToChain(selectedName,0x0000000000000000000000000000000000000000,selectedField,selectedAtk,100);
+    }
+
+
+    function sumWeight(uint[] memory _array) public view returns(uint) {
+        uint S = 0x0;
+         for(uint i;i < _array.length;i++){
+            S += _array[i];
+        }
+        return S;
     }
 
 
@@ -38,3 +92,8 @@ contract Eirbmon{
 
 
 }
+
+
+// lorsque le joueur commence, il a les choix entre 3 eirbmons, 
+// lorsqu'il en choisit 1, il en devient le proprio et cela créé alors 1 nouvel Eirbmon
+// qui dépend : du hash du block précédent, de l'adresse du joueur fois id de l'eirbmon modulo 10, et le 3e chiffre et la multi des 2 premiers modulo 10
--- test/Eirbmon.js
+++ test/Eirbmon.js
@@ -1,19 +1,28 @@
 var Eirbmon = artifacts.require("./Eirbmon.sol");
 var pokeInstance;
-it("initialises with two pokemons",function(){
-    return Eirbmon.deployed().then(instance =>
-                          instance.eirbmonsCount()).
-                          then(count => assert(count,3))
 
-});
-
-it("initialises with the pokemons with correct values",function(){
+it("d",function(){
     return Eirbmon.deployed().then(instance =>{
-                          pokeInstance = instance;
-                          return pokeInstance._Eirbmons(0);}).
-                          then(pokemon => {
-                            assert.equal(pokemon[0],0);
-                            assert.equal(pokemon[1],"Gribouille");
-                        })
+        for (let index = 0; index < 100; index++) {
+            instance.generateAnNewEirbmon(index).then(count => console.log(count))
+        }
+        })                      
 
 });
+// it("initialises with two pokemons",function(){
+//     return Eirbmon.deployed().then(instance =>
+//                           instance.eirbmonsCount()).
+//                           then(count => assert(count,3))
+
+// });
+
+// it("initialises with the pokemons with correct values",function(){
+//     return Eirbmon.deployed().then(instance =>{
+//                           pokeInstance = instance;
+//                           return pokeInstance._Eirbmons(0);}).
+//                           then(pokemon => {
+//                             assert.equal(pokemon[0],0);
+//                             assert.equal(pokemon[1],"Gribouille");
+//                         })
+
+// });
