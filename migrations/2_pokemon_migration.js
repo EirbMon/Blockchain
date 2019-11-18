fec7552eb2561306087b58b24ca1a96f525d3207
Atia <abdssamadatia97@gmail.com>
2019-11-18 12:29:57 +0100

    config blockchain to connect to  front

--- migrations/2_pokemon_migration.js
+++ migrations/2_pokemon_migration.js
@@ -1,5 +1,12 @@
 var Eirbmon = artifacts.require("./Eirbmon.sol");
+const fs = require('fs');
 
-module.exports = function(deployer) {
-  deployer.deploy(Eirbmon);
+module.exports =  async (deployer, network, accounts) => {
+  await deployer.deploy(Eirbmon);
+
+  const metaDataFile = `${__dirname}/../build/contracts/Eirbmon.json`
+  const metaData = require(metaDataFile)
+  metaData.networks[deployer.network_id] = {}
+  metaData.networks[deployer.network_id].address = Eirbmon.address
+  fs.writeFileSync(metaDataFile, JSON.stringify(metaData, null, 4))
 };
