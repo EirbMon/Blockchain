f4095df0f64099c5efe5b0a1a0534f008727f3ef
atiaabdessamad <abdssamadatia97@gmail.com>
2019-11-18 22:23:09 +0100

    configure truffle

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,12 @@
 var Migrations = artifacts.require("./Migrations.sol");
+const fs = require('fs');
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+module.exports = async (deployer, network, accounts) => {
+  await deployer.deploy(Migrations);
+
+  const metaDataFile = `${__dirname}/../build/contracts/Migrations.json`
+  const metaData = require(metaDataFile)
+  metaData.networks[deployer.network_id] = {}
+  metaData.networks[deployer.network_id].address = Migrations.address
+  fs.writeFileSync(metaDataFile, JSON.stringify(metaData, null, 4))
 };
