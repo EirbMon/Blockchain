e6b736f541b327b7a10556f99361c61d90abd907
atiaabdessamad <abdssamadatia97@gmail.com>
2019-11-19 14:23:49 +0100

    solve

--- truffle-config.js
+++ truffle-config.js
@@ -5,7 +5,7 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "5888" // Match any network id
+      network_id: "*" // Match any network id
     },
     develop: {
       port: 8545
