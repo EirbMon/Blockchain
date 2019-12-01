e3e5de2fe0b7db6abde83b0aa4ea64f3c974f3a3
dcognet <36398384+dcognet@users.noreply.github.com>
2019-12-01 17:56:50 +0100

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -5,8 +5,7 @@ module.exports = {
     development: {
       host: "192.168.0.23", //mettre 127.0.0.1 en local
       port: 8545,
-      network_id: "*", // Match any network id
-      from:"0xa838a806D4BA343d88e1DFceF2af437F9F0880c4"
+      network_id: "*"// Match any network id
     },
     develop: {
       port: 8545
