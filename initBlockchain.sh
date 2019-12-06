4bb63fcf667bd9ff34147e3931f7798e791509ad
atiaabdessamad <abdssamadatia97@gmail.com>
2019-12-06 20:44:41 +0100

    add truffle test migrate to init File

--- initBlockchain.sh
+++ initBlockchain.sh
@@ -3,8 +3,8 @@ Front_Directory='../Front'
 FrontBuildContracts_Directory='/src/build/contracts/' 
 Back_Directory='../Back'
 BackBuildContracts_Directory='/build/contracts/' 
-#truffle migrate --reset
-#truffle test
+truffle migrate --reset
+truffle test
 cp build/contracts/* $Front_Directory$FrontBuildContracts_Directory
 echo "Eirbmon.js copied in :" $Front_Directory$FrontBuildContracts_Directory
 cp build/contracts/* $Back_Directory$BackBuildContracts_Directory
