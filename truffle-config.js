4fffc808f23621cbe257bf6fcca38875ddfbed75
Abdessamad <36398441+atiaabdessamad@users.noreply.github.com>
2020-01-16 16:20:44 +0100

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -3,7 +3,7 @@ module.exports = {
   // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "192.168.1.40", //mettre 127.0.0.1 en local
+      host: "192.168.0.23", //mettre 127.0.0.1 en local
       port: 8545,
       network_id: "5900"// Match any network id
     },
